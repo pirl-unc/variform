fc19dcdfa32f8a5cf74c42569c400190e779f2a7
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-03-29 17:16:01 -0400

    capture system exit

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,10 @@
 from .common import eq_ 
 from variform.cli import main
+import pytest
 
 def test_version():
-    main("--version")
-    
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+         main("--version")
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+   
