26082d0480db1c5cb3a827baf06ee809f96e336a
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-03-29 17:07:19 -0400

    split argv

--- variform/cli.py
+++ variform/cli.py
@@ -4,6 +4,9 @@ def variant_stats():
     pass
 
 def main(argv=None):
+    if type(argv) is str:
+        argv = argv.split()
+        
     defopt.run({
         "variant-stats": variant_stats}, argv=argv)
     
