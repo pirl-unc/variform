7c0168322926ee27708065e899c9e8a91d3316d5
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-04-08 18:23:27 -0400

    using argh parser

--- variform/cli.py
+++ variform/cli.py
@@ -1,13 +1,14 @@
 import argh 
-from .version import print_version 
+
+from .version import version_string 
 
 
 def main(argv=None):
     if isinstance(argv, str):
         argv = argv.split()
-    
     parser = argh.ArghParser()
     parser.prog = 'variform'
-    parser.add_commands([print_version])
+    parser.add_argument("--version", action="version", version=version_string)
+    # parser.add_commands([])
     parser.dispatch(argv=argv, output_file=None)
     
--- variform/version.py
+++ variform/version.py
@@ -1,13 +1,24 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 __version__ = "0.0.0"
 
+version_string = f"v{__version__}"
 
 def print_version():
-    print(f"v{__version__}")
-
+    print(version_string)
 
 def print_name_and_version():
-    print(f"Variform v{__version__}")
-
+    print(f"Variform {version_string}")
 
 if __name__ == "__main__":
     print_version()
