796c77d86df3aa313c63ebd138a0c0f6bfd89b3c
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-04-09 12:20:30 -0400

    working on making the CLI config load/save

--- variform/cli.py
+++ variform/cli.py
@@ -1,7 +1,18 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argh 
 
 from .version import version_string 
-
 
 def main(argv=None):
     if isinstance(argv, str):
@@ -9,6 +20,10 @@ def main(argv=None):
     parser = argh.ArghParser()
     parser.prog = 'variform'
     parser.add_argument("--version", action="version", version=version_string)
+    parser.add_argument("--save-config", default=None, help="Save the configuration to a file")
+    parser.add_argument("--config", default=None, help="Load the configuration from a file")
+
+
     # parser.add_commands([])
     parser.dispatch(argv=argv, output_file=None)
     
